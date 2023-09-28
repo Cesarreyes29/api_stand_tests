4df9d28ed81ef0d2bc2c454b97396108eaf5f8d9
César Reyes <cesarreyes@MacBook-Air-de-Cesar.local>
2023-09-27 23:42:34 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = 'https://343e2ae7-4cee-4243-9165-54cf62d2d915.serverhub.tripleten-services.com'
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits"
